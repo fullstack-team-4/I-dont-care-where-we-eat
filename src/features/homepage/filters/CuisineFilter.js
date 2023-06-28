d29850d997852a915ad9fce4a4448b20c04f5670
rend1027 <rend1027@yahoo.com>
2023-06-28 13:51:02 -0400

    create filter components

---
+++ src/features/homepage/filters/CuisineFilter.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import { Text } from 'react-native-paper'
+
+const CuisineFilter = () => {
+  return (
+    <View>
+        <Text>CuisineFilter</Text>
+    </View>
+  )
+}
+
+export default CuisineFilter
---
+++ src/features/homepage/filters/PriceFilter.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const PriceFilter = () => {
+  return (
+    <View>
+        <Text>Price Filter</Text>
+    </View>
+  )
+}
+
+export default PriceFilter
