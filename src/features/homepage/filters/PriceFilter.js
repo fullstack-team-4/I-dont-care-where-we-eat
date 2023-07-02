56340d3c9cd316d8640073fa07e2a9b5bc5d6386
rend1027 <rend1027@yahoo.com>
2023-07-02 19:35:10 -0400

    Add filter options to PriceFilter

--- src/features/homepage/filters/PriceFilter.js
+++ src/features/homepage/filters/PriceFilter.js
@@ -1,10 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, { useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 
 const PriceFilter = () => {
+  const prices = ["$", "$$", "$$$", "$$$$"];
+  const [selectedPrice, setSelectedPrice] = useState(null);
+
+  const selectPrice = (selected) => {
+    setSelectedPrice(selected);
+  };
+
   return (
-    <View>
-      <Text>Price Filter</Text>
+    <View
+      style={{
+        flex: 1,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      {prices.map((price, index) => (
+        <TouchableOpacity
+          key={index}
+          style={{
+            backgroundColor: price === selectedPrice ? "blue" : "gray",
+            marginBottom: 10,
+            borderRadius: 10,
+            padding: 10,
+            alignItems: "center",
+          }}
+          onPress={() => selectPrice(price)}
+        >
+          <Text style={{ color: "white" }}>{price}</Text>
+        </TouchableOpacity>
+      ))}
+      <TouchableOpacity
+        style={{
+          backgroundColor: "gray",
+          marginBottom: 10,
+          borderRadius: 10,
+          padding: 10,
+          alignItems: "center",
+        }}
+        onPress={() => setSelectedPrice(null)}
+      >
+        <Text style={{ color: "white" }}>Reset</Text>
+      </TouchableOpacity>
     </View>
   );
 };
