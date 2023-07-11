68c69677ac9bd5dc748e4165aecb2caf263a528d
Angelgonzalez98 <gonzalez.angel8991@gmail.com>
2023-07-10 18:12:19 -0700

    added google icon

--- src/features/homepage/randomButton.js
+++ src/features/homepage/randomButton.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { TouchableOpacity, Image, Text, View, ScrollView } from "react-native";
+import { TouchableOpacity, Image, Text, View, ScrollView, Linking } from "react-native";
 import styled from "styled-components/native";
 import { GOOGLE_MAPS_API_KEY } from '@env';
-import CuisineFilter from "./filters/CuisineFilter";
+import star from "../../../assets/star";
+import { SvgXml } from "react-native-svg";
 
 const ButtonContainer = styled(View)`
   flex: 1;
@@ -28,13 +29,36 @@ const RestaurantImage = styled(Image)`
   shadow-offset: 1px 1px;
   shadow-radius: 4px;
 `;
+
+const Title = styled(Text)`
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+const InfoText = styled(Text)`
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 8px;
+`;
+
+const LinkIcon = styled(Image)`
+  width: 24px;
+  height: 24px;
+  margin-right: 8px;
+`;
+
+const RowContainer = styled(View)`
+  flex-direction: row;
+  align-items: center;
+`;
+
 const ChangeFateText = styled(Text)`
   margin-top: 30px;
   font-size: 16px;
   font-weight: bold;
   color: red;
 `;
-
 
 const RandomButton = ({ restaurants }) => {
   const [restaurant, setRandomRestaurant] = useState(null);
@@ -52,6 +76,37 @@ const RandomButton = ({ restaurants }) => {
     generateRandomRestaurant();
   };
 
+  const openGoogleMaps = () => {
+    if (restaurant && restaurant.reference) {
+      const url = `https://www.google.com/maps/place/?q=place_id:${restaurant.vicinity}`;
+      // Open the URL in a new window or tab
+      Linking.openURL(url);
+    }
+  };
+
+  const openPhoneNumber = () => {
+    if (restaurant && restaurant.formatted_phone_number) {
+      const phoneNumber = restaurant.formatted_phone_number;
+      // Open the phone number in the device's dialer
+      // Adjust this based on your platform (iOS/Android)
+      Linking.openURL(`tel:${phoneNumber}`);
+    }
+  };
+
+  const getPriceLevel = (level) => {
+    let dollarSigns = "";
+    for (let i = 0; i < level; i++) {
+      dollarSigns += "$";
+    }
+    return dollarSigns;
+  };
+  const getRatingsLevel = (level) => {
+    const starComponents = Array.from({ length: Math.floor(level) }, (_, index) => (
+      <SvgXml key={index} xml={star} width={20} height={20} />
+    ));
+    return starComponents;
+  };
+
   return (
     <ButtonContainer>
       {restaurant ? (
@@ -66,11 +121,22 @@ const RandomButton = ({ restaurants }) => {
               />
             ))}
           </ScrollView>
-          <Text style={{ fontSize: 30, fontWeight: 'bold' }}>{restaurant.name}</Text>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>{restaurant.vicinity}</Text>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Rating: {restaurant.rating} / 5     ({restaurant.user_ratings_total})</Text>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Opening Hours: {restaurant.opening_hours && restaurant.opening_hours.open_now ? 'Open' : 'Closed'}</Text>
-          <Text style={{ fontSize: 16, fontWeight: 'bold' }}>Distance: {restaurant.distance} meters</Text>
+          <Title>{restaurant.name}</Title>
+          <InfoText>{getPriceLevel(restaurant.price_level)} {restaurant.distance}</InfoText>
+          <InfoText>
+            {getRatingsLevel(restaurant.rating)} {restaurant.rating}      Reviews:({restaurant.user_ratings_total})
+          </InfoText>
+          <InfoText>{restaurant.vicinity}</InfoText>
+          <InfoText>({restaurant.opening_hours && restaurant.opening_hours.open_now ? 'Open' : 'Closed'})</InfoText>
+
+          <RowContainer>
+            <TouchableOpacity onPress={openGoogleMaps}>
+              <LinkIcon source={require("../../../assets/gmap.png")} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={openPhoneNumber}>
+              <LinkIcon source={require("../../../assets/phone.webp")} />
+            </TouchableOpacity>
+          </RowContainer>
           <ChangeFateText onPress={handlePress}>Click here to change your fate ¯\_(ツ)_/¯ </ChangeFateText>
         </View>
       ) : (
