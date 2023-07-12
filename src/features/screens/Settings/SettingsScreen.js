8316afbbbdde70bf9457a3410dc6c8bf6864962c
Frank S. Latino <100977147+FSL818@users.noreply.github.com>
2023-07-12 13:18:44 -0400

    Edit to Settings

--- src/features/screens/Settings/SettingsScreen.js
+++ src/features/screens/Settings/SettingsScreen.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, Linking, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Linking, TouchableOpacity, ScrollView, ImageBackground, StyleSheet } from 'react-native';
 import { Auth } from 'aws-amplify';
+
+const backgroundImage = require('../../../../assets/home-backg.avif');
 
 export const SettingsScreen = () => {
   const [username, setUsername] = useState('');
@@ -55,75 +57,100 @@ export const SettingsScreen = () => {
   };
 
   return (
-    <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
-      <View style={{ flex: 1 }}>
-        <Text style={{
-          fontSize: 30,
-          alignSelf: 'center',
-          fontWeight: 'bold',
-          marginVertical: 70,
-          textShadowColor: 'gold',
-          textShadowOffset: { width: 2, height: 2 },
-          textDecorationLine: 'underline',
-          textShadowRadius: 6,
-          textDecorationStyle: 'solid', // Added to ensure underline is solid
-        }}>
+    <ImageBackground source={backgroundImage} style={styles.background}>
+      <ScrollView contentContainerStyle={styles.container}>
+        <Text style={styles.welcomeText}>
           Welcome, {username}
         </Text>
-        <Text
-          style={{
-            fontSize: 24,
-            textAlign: 'center',
-            fontWeight: 'bold',
-            textShadowColor: 'rgba(0, 0, 0, 0.5)',
-            textShadowOffset: { width: 2, height: 2 },
-            textShadowRadius: 2,
-            textDecorationLine: 'underline',
-            marginVertical: 20,
-            textShadowColor: 'gold',
-            textShadowOffset: { width: 2, height: 2 },
-            textShadowRadius: 6,
-            textDecorationLine: 'underline'
-          }}
-        >
+        <Text style={styles.heading}>
           App Developers
         </Text>
 
-        <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+        <View style={styles.profilesContainer}>
           {linkedInProfiles.map((profile, index) => (
             <TouchableOpacity
               key={index}
               onPress={() => openLinkedInProfile(profile.url)}
-              style={{
-                backgroundColor: 'grey',
-                paddingHorizontal: 10,
-                paddingVertical: 5,
-                borderRadius: 5,
-                marginVertical: 5,
-              }}
+              style={styles.profileButton}
             >
-              <Text style={{ color: 'white' }}>{profile.name}</Text>
+              <Text style={styles.profileButtonText}>{profile.name}</Text>
             </TouchableOpacity>
           ))}
         </View>
 
-        <Text
-          onPress={signOut}
-          style={{
-            width: '100%',
-            textAlign: 'center',
-            color: 'red',
-            marginVertical: 100,
-            fontSize: 30,
-            textShadowColor: 'rgba(0, 0, 0, 0.5)',
-            textShadowOffset: { width: 2, height: 2 },
-            textShadowRadius: 1,
-            textDecorationLine: 'underline'
-          }}
-        >
-          Sign Out
-        </Text>
-      </View>
-    </ScrollView>
+        <TouchableOpacity onPress={signOut} style={styles.signOutButton}>
+          <Text style={styles.signOutButtonText}>Sign Out</Text>
+        </TouchableOpacity>
+      </ScrollView>
+    </ImageBackground>
   );
 };
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  container: {
+    flexGrow: 1,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  welcomeText: {
+    fontSize: 30,
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    marginVertical: 70,
+    textShadowColor: 'gold',
+    textShadowOffset: { width: 2, height: 2 },
+    textDecorationLine: 'underline',
+    textShadowRadius: 6,
+    textDecorationStyle: 'solid',
+    color: 'white',
+  },
+  heading: {
+    fontSize: 24,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 2,
+    textDecorationLine: 'underline',
+    marginVertical: 20,
+    textShadowColor: 'gold',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 6,
+    textDecorationLine: 'underline',
+    color: 'white',
+  },
+  profilesContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileButton: {
+    backgroundColor: 'grey',
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    borderRadius: 5,
+    marginVertical: 5,
+  },
+  profileButtonText: {
+    color: 'white',
+  },
+  signOutButton: {
+    width: '100%',
+    textAlign: 'center',
+    color: 'red',
+    marginVertical: 100,
+    fontSize: 30,
+    textShadowColor: 'rgba(0, 0, 0, 0.5)',
+    textShadowOffset: { width: 2, height: 2 },
+    textShadowRadius: 1,
+    textDecorationLine: 'underline',
+  },
+  signOutButtonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+});
