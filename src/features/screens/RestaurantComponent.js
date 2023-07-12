228b50d2bd313361324d453f2b16492a258a077b
Angelgonzalez98 <gonzalez.angel8991@gmail.com>
2023-07-12 08:20:35 -0700

    fixed api variable

--- src/features/screens/RestaurantComponent.js
+++ src/features/screens/RestaurantComponent.js
@@ -6,6 +6,7 @@ import { SvgXml } from "react-native-svg";
 import star from "../../../assets/star";
 import open from "../../../assets/open";
 import { isNotEmittedStatement } from "typescript";
+import { GOOGLE_MAP_API_KEY } from '@env'
 
 const RestaurantCard = styled(Card)`
   background-color: #F96666;
@@ -64,7 +65,7 @@ const LinkIcon = styled(Image)`
 
 export const RestaurantInfoCard = ({ name, rating, isClosedTemporarily, opening_hours, vicinity, photos, user_ratings_total }) => {
   const reference = photos[0]?.photo_reference;
-  const apiKey = "AIzaSyB4mhr5mWVDKq3VQrGmloq91Be4KTi9LT8";
+  const apiKey = GOOGLE_MAP_API_KEY;
   let photoUrl = null;
 
   if (reference && apiKey) {
